40ad8cdd550e0c4b973fefa87a4f9df967ca4d33
NickSoltau <nick.soltau@gmail.com>
2026-04-08 13:31:45 -0400

    try again

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Search from "./pages/Search/Search";
